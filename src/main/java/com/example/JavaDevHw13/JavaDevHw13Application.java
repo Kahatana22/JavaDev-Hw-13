4cf2fd7c37a3e2ad967595380ce5d90bd5dbfa66
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-21 22:47:02 +0300

    Create JavaDevHw13Application.java

---
+++ src/main/java/com/example/JavaDevHw13/JavaDevHw13Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaDevHw13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevHw13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevHw13Application.class, args);
+	}
+
+}
